b398c2bdb19258e925acbec765780b3cfbc6ba35
yukiterunamaana <74673274+yukiterunamaana@users.noreply.github.com>
2022-12-29 18:48:39 +0000

    added views/recipe/list, achieved first screen display

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "recipe#list"
+  get "recipe/create", to: "recipe#new"
 end
